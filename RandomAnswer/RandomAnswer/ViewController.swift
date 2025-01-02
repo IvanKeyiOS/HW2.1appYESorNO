3074bd9a792bbc04181161ae362dafd6718925f6
IvanKeyiOS <ivanbeat888@gmail.com>
2025-01-02 01:36:05 +0100

    changes

--- RandomAnswer/RandomAnswer/ViewController.swift
+++ RandomAnswer/RandomAnswer/ViewController.swift
@@ -11,14 +11,24 @@ class ViewController: UIViewController {
     @IBOutlet weak var answerLabel: UILabel!
     @IBOutlet weak var answerButton: UIButton!
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+//        answerButton.layer.cornerRadius = 16
+        answerButton.layer.shadowColor = UIColor.systemCyan.cgColor
+        answerButton.layer.shadowOpacity = 10
+//        answerButton.setTitle("Ask", for: .normal)
+//        answerButton.backgroundColor = UIColor.systemRed
         
-        answerButton.layer.cornerRadius = 16
+       
     }
+    
     @IBAction func answerButtonAction(_ sender: Any) {
         let answer = Bool.random()
         answerLabel.text = ""
+        answerLabel.textColor = UIColor.white
+        answerLabel.shadowColor = UIColor.orange
+        answerLabel.shadowOffset = CGSize(width: -2, height: -2)
         
         DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
             self.answerLabel.text = answer ? "YES" : "NO"
